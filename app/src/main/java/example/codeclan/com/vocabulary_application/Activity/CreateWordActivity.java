a243545b85cf47c1e6c133c1568c0da021352bca
Eric Carles <carles.eric@wanadoo.fr>
2018-01-31 11:24:10 +0000

    CreateWordActivity : view items declaration and onclick method bodies created

--- app/src/main/java/example/codeclan/com/vocabulary_application/Activity/CreateWordActivity.java
+++ app/src/main/java/example/codeclan/com/vocabulary_application/Activity/CreateWordActivity.java
@@ -2,6 +2,7 @@ package example.codeclan.com.vocabulary_application.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
@@ -30,6 +31,16 @@ public class CreateWordActivity extends AppCompatActivity {
     private EnumWordTypeAdapter enumWordTypeAdapter;
 
 
+    /* Add/Modify a meaning for a word */
+    private TextView addMeaningMeaning;
+    private TextView addMeaningExample;
+    private TextView addMeaningSynonyms;
+    private TextView addMeaningAntonyms;
+    private Button addMeaningCancelButton;
+    private Button addMeaningSaveButton;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +59,32 @@ public class CreateWordActivity extends AppCompatActivity {
         enumWordTypeAdapter = new EnumWordTypeAdapter(this, new ArrayList<>(Arrays.asList(EnumWordType.values())));
         listWordTypesSpinner.setAdapter(enumWordTypeAdapter);
 
+        /* Add/Modify a meaning for a word */
+        addMeaningMeaning           = findViewById(R.id.addMeaningMeaning);
+        addMeaningExample           = findViewById(R.id.addMeaningExample);
+        addMeaningSynonyms          = findViewById(R.id.addMeaningSynonyms);
+        addMeaningAntonyms          = findViewById(R.id.addMeaningAntonyms);
+        addMeaningCancelButton      = findViewById(R.id.addMeaningCancelButton);
+        addMeaningSaveButton        = findViewById(R.id.addMeaningSaveButton);
 
     }
+
+    public void onClickCreateWordSaveButton(View button){
+
+    }
+
+    public void onclickCreateWordCancelButton(View button){
+
+    }
+
+    public void onClickAddMeaningSaveButton(View button){
+
+    }
+
+    public void onClickAddMeaningCancelButton(View button){
+
+    }
+
+
+
 }
