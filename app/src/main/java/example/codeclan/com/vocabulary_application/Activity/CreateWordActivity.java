986630f152fd24f0a3b53be0b1f4fd2cae89749a
Eric Carles <carles.eric@wanadoo.fr>
2018-01-31 10:24:13 +0000

    CreateWordActivity AddMeaning FrameLayout added

--- app/src/main/java/example/codeclan/com/vocabulary_application/Activity/CreateWordActivity.java
+++ app/src/main/java/example/codeclan/com/vocabulary_application/Activity/CreateWordActivity.java
@@ -2,14 +2,52 @@ package example.codeclan.com.vocabulary_application.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import example.codeclan.com.vocabulary_application.Database.WordsRoomDatabase;
+import example.codeclan.com.vocabulary_application.Enumerations.EnumWordType;
 import example.codeclan.com.vocabulary_application.R;
+import example.codeclan.com.vocabulary_application.ViewAdapter.EnumWordTypeAdapter;
 
 public class CreateWordActivity extends AppCompatActivity {
+
+
+    private TextView wordSpellingTextView;
+    private Spinner listWordTypesSpinner;
+    private Button addMeaningButton;
+    private ListView wordMeaningsListView;
+    private Button saveWordButton;
+    private Button cancelWordButton;
+
+    private WordsRoomDatabase database;
+
+    private EnumWordTypeAdapter enumWordTypeAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_word);
+
+        database = WordsRoomDatabase.getDatabase(this);
+
+        wordSpellingTextView = findViewById(R.id.createWordSpelling);
+        listWordTypesSpinner = findViewById(R.id.createWordTypesList);
+        addMeaningButton     = findViewById(R.id.createWordAddMeaning);
+        wordMeaningsListView = findViewById(R.id.createWordMeaningsList);
+        saveWordButton       = findViewById(R.id.createWordSaveButton);
+        cancelWordButton     = findViewById(R.id.createWordCancelButton);
+
+
+        enumWordTypeAdapter = new EnumWordTypeAdapter(this, new ArrayList<>(Arrays.asList(EnumWordType.values())));
+        listWordTypesSpinner.setAdapter(enumWordTypeAdapter);
+
+
     }
 }
