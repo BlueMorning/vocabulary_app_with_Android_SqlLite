22b694e225a675a036cbaebe1a22a8cba738bf8a
Eric Carles <carles.eric@wanadoo.fr>
2018-01-31 10:40:35 +0000

    WordModel : addMeaning and saveWord methods added

--- app/src/main/java/example/codeclan/com/vocabulary_application/Model/WordModel.java
+++ app/src/main/java/example/codeclan/com/vocabulary_application/Model/WordModel.java
@@ -1,6 +1,9 @@
 package example.codeclan.com.vocabulary_application.Model;
 
+import java.util.ArrayList;
+
 import example.codeclan.com.vocabulary_application.Database.WordsRoomDatabase;
+import example.codeclan.com.vocabulary_application.Entity.MeaningEntity;
 import example.codeclan.com.vocabulary_application.Entity.StatsEntity;
 import example.codeclan.com.vocabulary_application.Entity.WordEntity;
 import example.codeclan.com.vocabulary_application.Enumerations.EnumMasteryLevel;
@@ -15,9 +18,14 @@ public class WordModel {
 
     private WordEntity  wordEntity;
     private StatsModel  statsModel;
+    private ArrayList<MeaningModel> meaningsList;
 
     public WordModel(WordEntity wordEntity){
         this.wordEntity = wordEntity;
+    }
+
+    public WordModel(String wordSpelling, EnumWordType enumWordType){
+        this.wordEntity = new WordEntity(enumWordType, wordSpelling, "");
     }
 
     public WordEntity getWordEntity(){
@@ -50,4 +58,34 @@ public class WordModel {
     public void setStatsModel(StatsModel statsModel) {
         this.statsModel = statsModel;
     }
+
+    public void addMeaning(String meaning, String example, String synonyms, String antonyms){
+        MeaningEntity meaningEntity = new MeaningEntity(this.wordEntity.getId(), meaning, example, synonyms, antonyms);
+        MeaningModel meaningModel   = new MeaningModel(meaningEntity);
+        meaningsList.add(meaningModel);
+    }
+
+    public void saveWord(WordsRoomDatabase db){
+
+        if(this.wordEntity.getId() == 0){
+            this.wordEntity.setId(db.wordDao().insertWord(this.wordEntity));
+        }
+        else{
+            db.wordDao().updateWord(this.wordEntity);
+        }
+
+        for(MeaningModel meaningModel : this.meaningsList){
+
+            if(meaningModel.getMeaningEntity().getId() == 0){
+                meaningModel.getMeaningEntity().setWordId(this.wordEntity.getId());
+                db.meaningDao().insertMeaning(meaningModel.getMeaningEntity());
+            }
+            else{
+                db.meaningDao().updateMeaning(meaningModel.getMeaningEntity());
+            }
+        }
+    }
+
+
+
 }
