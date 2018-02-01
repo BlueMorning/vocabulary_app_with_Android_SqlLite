24b532875e35e9e031818d2a4cdae545d97bab2f
Eric Carles <carles.eric@wanadoo.fr>
2018-02-01 17:26:16 +0000

    EnumTrainingPriority added

---
+++ app/src/main/java/example/codeclan/com/vocabulary_application/Enumerations/EnumTrainingPriority.java
@@ -0,0 +1,24 @@
+package example.codeclan.com.vocabulary_application.Enumerations;
+
+/**
+ * Created by horizon on 01/02/2018.
+ */
+
+public enum EnumTrainingPriority {
+
+    LOW("Low"),
+    MEDIUM("Medium"),
+    HIGH("High");
+
+    String label;
+
+    EnumTrainingPriority(String label){
+        this.label = label;
+    }
+
+    String getLabel(){
+        return this.label;
+    }
+
+
+}
--- app/src/main/java/example/codeclan/com/vocabulary_application/Enumerations/EnumTrainingStatus.java
+++ app/src/main/java/example/codeclan/com/vocabulary_application/Enumerations/EnumTrainingStatus.java
@@ -3,8 +3,18 @@ package example.codeclan.com.vocabulary_application.Enumerations;
 
 public enum EnumTrainingStatus {
 
-    ALL,
-    ONGOING,
-    DONE;
+    ALL("All"),
+    ONGOING("Ongoing"),
+    DONE("Done");
+
+    private String label;
+
+    EnumTrainingStatus(String label){
+        this.label = label;
+    }
+
+    String getLabel(){
+        return this.label;
+    }
 
 }
